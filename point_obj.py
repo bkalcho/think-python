6c35dad34906be3a882bd3b6387774223035af30
Bojan Kalicanin <bojan.g.kalicanin@gmail.com>
2016-10-29 11:37:49 +0200

    add solutions to the chapter Classes and methods.

---
+++ point_obj.py
@@ -0,0 +1,50 @@
+# Author: Bojan G. Kalicanin
+# Date: 29-Oct-2016
+# 1. Write an init method for the Point class that takes x and y as
+# optional parameters and assigns them to the corresponding attributes.
+# 2. Write a str method for the Point class. Create a Point object and 
+# print it.
+# 3. Write an add method for the Point class.
+# 4. Write an add method for Points that works with either a Point
+# object or a tuple.
+
+class Point(object):
+    """Represents a point in a 2-D world."""
+    
+    def __init__(self, x=0, y=0):
+        """Initialization of the class attributes at instantiation."""
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        """String representation of the object, used when printing."""
+        return "({0:.2f}, {1:.2f})".format(self.x, self.y)
+        
+    def __add__(self, other):
+        """Addition operator overloading."""
+        if isinstance(other, Point):
+            return self.adding_points(other)
+        else:
+            return self.adding_tuple(other)
+    
+    def __rad__(self, other):
+        """Right side addition."""
+        return self.__add__(other)
+            
+    def adding_points(self, other):
+        """Method for adding two points."""
+        sum = Point()
+        sum.x = self.x + other.x
+        sum.y = self.y + other.y
+        return sum
+        
+    def adding_tuple(self, other):
+        """Adding point to the point represented by tuple."""
+        sum = Point()
+        sum.x = self.x + other[0]
+        sum.y = self.y + other[1]
+        return sum
+    
+        
+point = Point(2.3456, 3.48793)
+print(point)
---
+++ time_obj.py
@@ -0,0 +1,82 @@
+# Author: Bojan G. Kalicanin
+# Date: 29-Oct-2016
+#
+
+class Time(object):
+    """Represents a Time."""
+    def __init__(self, hour=0, minute=0, second=0):
+        """
+        Initialization of the class attributes at object creation
+        (instantiation).
+        """
+        self.hour = hour
+        self.minute = minute
+        self.second = second
+        
+    def __str__(self):
+        """Special method that returns string representation of the object."""
+        return "{0:02d}:{1:02d}:{2:02d}".format(self.hour, self.minute,
+            self.second)
+            
+    def __add__(self, other):
+        """Overloading of the addition operator (+)."""
+        if isinstance(other, Time):
+            return self.add_time(other)
+        else:
+            return self.increment(other)
+            
+    def __rad__(self, other):
+        """
+        Right-side add. This method is invoked when a Time object
+        appears on the right side of the + operator.
+        """
+        return self.__add__(other)
+            
+    def print_time(self):
+        """Print time in the usual format HH:MM:SS."""
+        print("{0:02d}:{1:02d}:{2:02d}".format(self.hour, self.minute,
+            self.second))
+            
+    def time_to_int(self):
+        """Convert time to integer, with base of 60."""
+        minutes = self.hour*60 + self.minute
+        seconds = minutes*60 + self.second
+        return seconds
+        
+    def add_time(self, other):
+        """Summing two time objects."""
+         seconds = self.time_to_int() + other.time_to_int()
+     return int_to_time(seconds)
+        
+    def increment(self, seconds):
+        """Increment a time for a given number of the seconds."""
+        seconds += self.time_to_int()
+        return int_to_time(seconds)
+        
+    def is_after(self, other):
+        """Check if some time is after another."""
+        return self.time_to_int() > other.time_to_int()
+        
+        
+def int_to_time(seconds):
+    time = Time()
+    minutes, time.second = divmod(seconds, 60)
+    time.hour, time.minute = divmod(minutes, 60)
+    return time
+            
+            
+start = Time()
+start.hour = 11
+start.minute = 59
+start.second = 30
+start.print_time()
+print(start.time_to_int())
+
+end = start.increment(1337)
+end.print_time()
+
+print(end.is_after(start))
+print(start)
+
+duration = Time(1, 35)
+print(start + duration)
