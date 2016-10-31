6f6625f381dbc7ccbd92e41a1360bbfbabd70b6d
Bojan Kalicanin <bojan.g.kalicanin@gmail.com>
2016-10-31 18:34:15 +0100

    add n-day to double_day.py

--- double_day.py
+++ double_day.py
@@ -8,22 +8,27 @@
 
 import datetime
 
+def n_day(b_day1, b_day2, n):
+    """Computes double day."""
+    if b_day1 > b_day2:
+        start_date = b_day1
+    else:
+        start_date = b_day2
+
+    for i in range(100*365):
+        c_date = start_date + datetime.timedelta(days=i)
+        d1 = c_date - b_day1
+        d2 = c_date - b_day2
+        if d1 > d2:
+            if d1.days == n*d2.days:
+                print(c_date.strftime('%d-%m-%Y'))
+        else:
+            if d2.days == n*d1.days:
+                print(c_date.strftime('%d-%m-%Y'))
+                
+                
 b_day1 = input("Input first birthday (DD-MM-YYYY HH:MI:SS)? ")
 b_day1 = datetime.datetime.strptime(b_day1, '%d-%m-%Y %H:%M:%S')
 b_day2 = input("Input second birthday (DD-MM-YYYY HH:MI:SS)? ")
 b_day2 = datetime.datetime.strptime(b_day2, '%d-%m-%Y %H:%M:%S')
-if b_day1 > b_day2:
-    start_date = b_day1
-else:
-    start_date = b_day2
-
-for i in range(100*365):
-    c_date = start_date + datetime.timedelta(days=i)
-    d1 = c_date - b_day1
-    d2 = c_date - b_day2
-    if d1 > d2:
-        if d1.days == 2*d2.days:
-            print(c_date.strftime('%d-%m-%Y'))
-    else:
-        if d2.days == 2*d1.days:
-            print(c_date.strftime('%d-%m-%Y'))
+n_day(b_day1, b_day2, 3)
